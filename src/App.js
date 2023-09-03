f68c1e92a486693e87fc300fba6b953c4faa412a
Ankit2099 <64951054+anshuUnity@users.noreply.github.com>
2023-09-03 20:46:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './Components/Register';
+import Login from './Components/Login';
+import Navigate from './Components/Navigate';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ChatArea from './Components/ChatArea';
+import Sidebar from './Components/Sidebar';
+
+
+function App() {
+  return (
+      // <>
+      //   <div className='chat-container'>
+      //   <Sidebar/>
+      //   <ChatArea/>
+      //   </div>
+      // </>
+    <BrowserRouter>
+      <Navigate/>
+      <Routes>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/chat' element={<><Sidebar/><ChatArea/></>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/MessageInput.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function MessageInput() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    const handleSendMessage = () => {
+        console.log("MESSAGE SEND")
+    }
+  return (
+    <div className='message-input'>
+        <textarea 
+            placeholder='Type your message'
+            value={inputValue}
+            onChange={handleInputChange}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+    </div>
+  )
+}
